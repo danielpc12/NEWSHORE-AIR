f4389a5a18112c60adc8c4a318c509347fdbe480
danielpc12 <danielpiedrahita123@hotmail.com>
2023-04-28 23:18:01 -0500

    Creación componente para formulario origen destino

--- src/app/components/form/form.component.ts
+++ src/app/components/form/form.component.ts
@@ -1,10 +1,24 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import {FormGroup, FormControl, Validators} from '@angular/forms'
 
 @Component({
   selector: 'app-form',
   templateUrl: './form.component.html',
   styleUrls: ['./form.component.css']
 })
-export class FormComponent {
+export class FormComponent implements OnInit{
 
+  flightsForm = new FormGroup({
+    origin: new FormControl('', Validators.required),
+    destination: new FormControl('', Validators.required)
+  })
+  constructor(){}
+
+  ngOnInit(): void {
+      
+  }
+
+  onSearch(form: any){
+    console.log(form)
+  }
 }
